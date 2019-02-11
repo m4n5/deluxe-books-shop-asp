0b3f233ae62b7ebde66e7248137e592d4210f426
Gazizov Runar <runar@yandex.ru>
2019-02-11 20:07:21 +0300

    Init commit

---
+++ deluxe-books-shop-asp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace deluxe_books_shop_asp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
